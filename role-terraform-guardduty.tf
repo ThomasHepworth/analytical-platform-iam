851440d251bc067ff223c7e505c73b5d5f5b18a8
Oliver Butterbach <oliver.butterbach@digital.justice.gov.uk>
2019-05-24 14:34:59 +0100

    Remove environment name from role names

--- role-terraform-guardduty.tf
+++ role-terraform-guardduty.tf
@@ -37,7 +37,7 @@ module "add_terraform_guardduty_role_in_landing" {
   source = "modules/role"
 
   assume_role_in_account_id = "${var.landing_account_id}"
-  role_name                 = "${var.terraform_guardduty_name}-${local.landing}"
+  role_name                 = "${var.terraform_guardduty_name}"
   landing_account_id        = "${var.landing_account_id}"
   role_policy               = "${data.aws_iam_policy_document.ap_terraform_guardduty.json}"
 }
@@ -47,7 +47,7 @@ module "add_terraform_guardduty_role_in_dev" {
   source = "modules/role"
 
   assume_role_in_account_id = "${var.ap_accounts["dev"]}"
-  role_name                 = "${var.terraform_guardduty_name}-${local.dev}"
+  role_name                 = "${var.terraform_guardduty_name}"
   landing_account_id        = "${var.landing_account_id}"
   role_policy               = "${data.aws_iam_policy_document.ap_terraform_guardduty.json}"
 }
@@ -57,7 +57,7 @@ module "add_terraform_guardduty_role_in_prod" {
   source = "modules/role"
 
   assume_role_in_account_id = "${var.ap_accounts["prod"]}"
-  role_name                 = "${var.terraform_guardduty_name}-${local.prod}"
+  role_name                 = "${var.terraform_guardduty_name}"
   landing_account_id        = "${var.landing_account_id}"
   role_policy               = "${data.aws_iam_policy_document.ap_terraform_guardduty.json}"
 }
@@ -67,7 +67,7 @@ module "add_terraform_guardduty_role_in_data" {
   source = "modules/role"
 
   assume_role_in_account_id = "${var.ap_accounts["data"]}"
-  role_name                 = "${var.terraform_guardduty_name}-${local.data}"
+  role_name                 = "${var.terraform_guardduty_name}"
   landing_account_id        = "${var.landing_account_id}"
   role_policy               = "${data.aws_iam_policy_document.ap_terraform_guardduty.json}"
 }
