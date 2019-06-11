1f26b5704105706e8dbb2f508185987b6217c863
Shojul Hassan <sh@serverless.digital>
2019-06-11 14:55:02 +0100

    use pipeline module/fix tfstate name

--- pipeline/master-pipeline.tf
+++ pipeline/master-pipeline.tf
@@ -1,8 +1,17 @@
+data "aws_iam_policy_document" "codebuild_policy" {
+  statement {
+    sid = "assumeLandingIamRole"
+    actions = ["sts:AssumeRole"]
+    resources = ["arn:aws:iam::*:role/landing-iam-role"]
+  }
+}
+
 module "master-pipeline" {
-  source = "../modules/pipeline"
+  source = "github.com/ministryofjustice/analytical-platform-pipeline"
   
   name = "iam-pipeline"
   pipeline_github_repo = "analytical-platform-iam"
   pipeline_github_owner = "ministryofjustice"
   pipeline_github_branch = "master"
+  codebuild_policy = "${data.aws_iam_policy_document.codebuild_policy.json}"
 }
