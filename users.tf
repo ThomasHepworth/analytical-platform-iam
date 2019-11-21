4c91cb739b3ba8ac08530b9c6c04c77618a7f0f9
Karik Isichei <karik.isichei@gmail.com>
2019-11-21 13:54:50 +0000

    Forgot to add myself as a user...

--- users.tf
+++ users.tf
@@ -29,6 +29,10 @@ resource "aws_iam_user" "calum" {
   name          = "calum.barnett@digital.justice.gov.uk"
   force_destroy = true
 }
+resource "aws_iam_user" "karik" {
+  name          = "karik.isichei@digital.justice.gov.uk"
+  force_destroy = true
+}
 
 resource "aws_iam_user" "robin" {
   name          = "robin.linacre@digital.justice.gov.uk"
