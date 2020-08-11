9bf73d02a49a9ed382ff6d3ac5e6b3ed302b5e96
calumabarnett <calum.barnett@digital.justice.gov.uk>
2020-08-11 19:22:42 +0100

    Create group to allow assumption of Pulumi base role

--- assume-data-engineering.tf
+++ assume-data-engineering.tf
@@ -16,3 +16,13 @@ module "add_restricted_admin_role_in_data_engineering" {
   landing_account_id = var.landing_account_id
   role_policy        = data.aws_iam_policy_document.restricted_admin.json
 }
+
+module "assume_pulumi_codebuild_service_role_in_data_engineering" {
+  source = "./modules/assume"
+
+  assumed_role_name         = "system/pulumi-codebuild-service-role"
+  assume_role_in_account_id = var.data_engineering_account_id
+  landing_account_id        = var.landing_account_id
+  group_name                = "pulumi-data-engineering"
+  users                     = module.data_engineering_team.user_names
+}
