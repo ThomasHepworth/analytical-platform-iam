beb79fc6b63e5a483674c5f8c50ec60296e56075
Dan Webb <dan.webb@damacus.io>
2020-08-14 15:02:38 +0100

    Remove the restricted_admin-prod role (#206)
    This role has been superseded from restricted-admin

--- assume-prod.tf
+++ assume-prod.tf
@@ -1,23 +1,3 @@
-module "assume_restricted_admin_in_prod" {
-  source = "./modules/assume"
-
-  assumed_role_name         = "${var.restricted_admin_name}-prod"
-  assume_role_in_account_id = var.ap_accounts["prod"]
-  source_account_id         = var.ap_accounts["landing"]
-  group_name                = "${var.restricted_admin_name}-prod"
-  users                     = module.analytical_platform_team.user_names
-}
-
-module "add_restricted_admin_role_in_prod" {
-  source    = "./modules/role"
-  providers = { aws = aws.prod }
-
-  role_name         = "${var.restricted_admin_name}-prod"
-  source_account_id = var.ap_accounts["landing"]
-  role_policy       = data.aws_iam_policy_document.restricted_admin.json
-  tags              = local.tags
-}
-
 module "add_audit_security_role_in_prod" {
   source    = "./modules/role"
   providers = { aws = aws.prod }
