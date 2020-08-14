79f52c48ff902d877089fec548badc80333f7f6c
Dan Webb <dan.webb@damacus.io>
2020-08-14 14:31:00 +0100

    Remove the pulumi-codebuild-service-role
    This role has been replaced with restricted-admin in the
    data-engineering account

--- assume-data-engineering.tf
+++
@@ -1,9 +0,0 @@
-module "assume_pulumi_codebuild_service_role_in_data_engineering" {
-  source = "./modules/assume"
-
-  assumed_role_name         = "system/pulumi-codebuild-service-role"
-  assume_role_in_account_id = var.ap_accounts["data_engineering"]
-  source_account_id         = var.ap_accounts["landing"]
-  group_name                = "pulumi-data-engineering"
-  users                     = module.data_engineering_team.user_names
-}
